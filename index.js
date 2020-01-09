9907f051edc9cbeb060b3ec3a99b539122fed09e
Carl Davidson <carl.davidson@hotmail.com>
2020-01-09 13:38:03 +1100

    temp finished backend using localhost for db

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config
+const morgan = require('morgan')
 
 //note: heroku wont always use port 5000 - as specfied below, heroku will reference the env else 5000
 const PORT = process.env.port || 5000
 
 const app = express()
 
-// const DB_URL = "mongodb://localhost:27017/lasagne-app-db"
 
 const dbConfig = {useNewUrlParser: true, useUnifiedTopology: true}
 
@@ -20,8 +20,18 @@ mongoose.connect(process.env.DB_URL, dbConfig, (err) => {
     }
 })
 
-// connect with index.js file in routes dir using middleware
+//middleware to access the body of the request
+app.user(express.json())
+app.use(morgan('dev'))
+//allows 2 different urls to talk with each other
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+// connect with index.js file in routes dir 
 app.use(require('./routes/index'))
+
+
 
 // listen
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
--- routes/lasagne-routes.js
+++ routes/lasagne-routes.js
@@ -4,6 +4,6 @@ const { index, create } = require('../controllers/lasagne-controller')
 
 //we are going to create routes for the lasagne page
 router.get('/', index)
-router.get('/create', create)
+router.post('/create', create)
 
 module.exports = router
